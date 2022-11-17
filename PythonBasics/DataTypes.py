f27f348a382c285de66e8ec80cfcadf389070827
Vineethac96 <41900084+Vineethac96@users.noreply.github.com>
2022-11-17 11:21:50 +0100

    Update DataTypes.py

--- PythonBasics/DataTypes.py
+++ PythonBasics/DataTypes.py
@@ -1,9 +1,15 @@
-#no need to declare any type of variable
-#A-Z,a-z,0-9
+'''no need to declare any type of variable, it is Dynamically Typed Language:
+Python is a dynamically-typed language. That means the type (for example- int, double, long, etc.) for a
+variable is decided at run time not in advance because of this feature we don’t need to specify the type of variable.
+the variable can be changed at any time further
+#A-Z,a-z,0-9 '''
 
 number_of_months = 12
 numberOfMonths = 12
 print(number_of_months,numberOfMonths)
+
+i = 10      #int
+i = 10+0.5  #float
 
 var1 = 100.0
 name = "Vineetha"
@@ -17,8 +23,9 @@ print(a,b,c)
 
 s1 = "hello World"
 print(s1[2])
+
+#SlICING
 print(s1[4:7]) #prints from 4th position to 6th position
 print(s1[3:]) #print entire string from 3rd position
 print(s1*3) #prints 3 times
 print(s1+" "+"Vinee") #concatenation
-
