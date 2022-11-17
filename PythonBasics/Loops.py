6b6253127f08e6f836aa94964488d3f9ca949028
Vineethac96 <41900084+Vineethac96@users.noreply.github.com>
2022-11-17 14:57:19 +0100

    Update Loops.py

--- PythonBasics/Loops.py
+++ PythonBasics/Loops.py
@@ -56,7 +56,24 @@ for i in range(1,5):
         print("*",end='')
     print()
 
+
 for i in range(1,5):
     for j in range(i):
         print(i,end='')
     print()
+
+
+rows = 5
+b = 0
+for i in range(rows, 0, -1):
+    b += 1
+    for j in range(1, i + 1):
+        print(b, end='')
+    print('\r')
+
+
+rows = 5
+for row in range(1, rows+1):
+    for column in range(1, row + 1):
+        print(column, end='')
+    print("")
