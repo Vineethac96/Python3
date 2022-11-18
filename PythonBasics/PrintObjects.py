1905938d22a319fcbe04d38df1b4315149d04184
Vineethac96 <41900084+Vineethac96@users.noreply.github.com>
2022-11-18 10:51:37 +0100

    Update PrintObjects.py

--- PythonBasics/PrintObjects.py
+++ PythonBasics/PrintObjects.py
@@ -3,7 +3,7 @@
 
 class Test:
 
-     def __init__(self,x,y):
+     def __init__(self,x,y):   #constructor
          self.x=x
          self.y=y
 
@@ -14,6 +14,10 @@ class Test:
          return "value of x is %s and y is %s" %(self.x,self.y)
 
 t = Test(10,20)
-print(t)
+print(t)  # string representation of object (reference of object)
 
-#if no str method is defined, then repr is called
+
+'''o/p: value of x is 10 and y is 20
+if no str method is defined, then repr is called'''
+
+
