c912457b31f7b14fb0770e86ebaef51132bc1890
Vineethac96 <41900084+Vineethac96@users.noreply.github.com>
2022-11-18 11:03:59 +0100

    Update Inheritance.py

--- PythonBasics/Inheritance.py
+++ PythonBasics/Inheritance.py
@@ -1,27 +1,29 @@
-class Person(object):   #object is superclass for all classes in python, default "()" means the same
+class Person(object):  # object is superclass for all classes in python, default "()" means the same
 
-    def __init__(self,name):
-        self.name=name
+    def __init__(self, name):
+        self.name = name
 
     def getName(self):
         return self.name
 
     def isEmployee(self):
-        return False
+        self.emp_age = 10
+        return self.emp_age
 
 
-class Employee(Person):                          #Empoyee is child class of super class Person
-    
-    def isEmployee(self):                        #method overriding
-        return True
+class Employee(Person):  # Empoyee is child class of super class Person
 
-#Test Person
-emp = Person("Vineetha")                          #as constructor has name parameter, we should mention name
+    def isEmployee(self):  # method overriding
+        self.emp_age = 20
+        return self.emp_age
+
+
+# Test Person
+emp = Person("Vineetha")  # as constructor has name parameter, we should mention name
 print(emp.name)
-print(emp.getName(), emp.isEmployee())
+print(emp.getName(), emp.isEmployee())     #isEmployee of Person Class
 
 emp1 = Employee("Blessy")
 print(emp1.name)
 print(emp1.getName())
-print(emp1.isEmployee())
-
+print(emp1.isEmployee())                    #isEmployee of Employee Class
